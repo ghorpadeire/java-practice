bb4af37169af126cb92a02f7105bfec250020fe9
Pranav Ghorpade <pranav.ghorpade3108@gmail.com>
2025-06-06 14:30:10 +0100

    Update readme

--- ControlFlowDemo.java
+++ ControlFlowDemo.java
@@ -5,6 +5,16 @@ public class ControlFlowDemo {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
 
+        // If-else statement 
+        if  (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
     }
 }
